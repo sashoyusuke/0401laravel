b5933501bedadbfba5274ed084ac0368f919856c
sashoyusuke <zyesi731@gmail.com>
2021-03-30 19:24:08 +0900

    first-commit

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,11 +7,7 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
         $items = Contact::all();
@@ -21,12 +17,7 @@ class ContactController extends Controller
         ], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   
     public function store(Request $request)
     {
         $item = new Contact;
@@ -39,24 +30,13 @@ class ContactController extends Controller
         ], 200);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Contact  $contact
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show(Contact $contact)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Contact  $contact
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(Request $request, Contact $contact)
     {
         $item = Contact::where('id', $contact->id)->first();
@@ -74,12 +54,7 @@ class ContactController extends Controller
         }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Contact  $contact
-     * @return \Illuminate\Http\Response
-     */
+   
     public function destroy(Contact $contact)
     {
         $item = Contact::where('id', $contact->id)->delete();
